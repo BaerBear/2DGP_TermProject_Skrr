25d174e67f9e6ad001b607b72b4e28590e37b907
BaerBear <turbogun03@gmail.com>
2025-10-19 01:03:47 +0900

    체크를 위한 IDLE 상태 생성

--- SKRR.py
+++ SKRR.py
@@ -1,5 +1,21 @@
 from pico2d import *
 
+class IDLE:
+    def __init__(self, skrr):
+        self.skrr = skrr
+
+    def enter(self):
+        self.skrr.frame = 0
+
+    def exit(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
 class SKRR:
     def __init__(self):
         self.x, self.y = get_canvas_width() // 2, get_canvas_height() // 2
