782e7a51c92e969f6391eb93fb20f7b26dbd37b2
BaerBear <turbogun03@gmail.com>
2025-10-19 01:06:28 +0900

    draw 완성

--- SKRR.py
+++ SKRR.py
@@ -14,10 +14,14 @@ class IDLE:
         pass
 
     def draw(self):
+        img = self.skrr.Idle_image[self.skrr.frame // 10 % 4]
         if self.skrr.face_dir == 1:
-            pass
-        else:
-            pass
+            img.clip_draw(0, 0, img.w, img.h, self.skrr.x, self.skrr.y,
+                          img.w * self.skrr.scale, img.h * self.skrr.scale)
+        elif self.skrr.face_dir == -1:
+            img.clip_composite_draw(0, 0, img.w, img.h, 0, 'h', self.skrr.x, self.skrr.y,
+                                    img.w * self.skrr.scale, img.h * self.skrr.scale)
+
 
 class SKRR:
     def __init__(self):
