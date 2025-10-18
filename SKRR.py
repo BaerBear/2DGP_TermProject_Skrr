9efadf13590d9584e52554a892023c15012df0f0
BaerBear <turbogun03@gmail.com>
2025-10-19 01:02:11 +0900

    SKRR 생성자 생성

--- SKRR.py
+++ SKRR.py
@@ -2,7 +2,9 @@ from pico2d import *
 
 class SKRR:
     def __init__(self):
-        pass
+        self.x, self.y = get_canvas_width() // 2, get_canvas_height() // 2
+        self.frame = 0
+        self.face_dir = 1
 
     def update(self):
         pass
