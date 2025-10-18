2e801ac6285d418a674bc228c00355863ace8448
BaerBear <turbogun03@gmail.com>
2025-10-19 01:18:39 +0900

    Idle 이미지 읽어오기

--- SKRR_Image_Loader.py
+++ SKRR_Image_Loader.py
@@ -5,3 +5,6 @@ class SKRR_Image_Loader:
     def __init__(self, state):
         self.images = []
         self.resource_path = os.path.join(os.path.dirname(__file__), r'..\2DGP_TermProject_Skrr\Resources\Image\Object', 'Skul')
+        if state == 'Idle':
+            for i in range(4):
+                self.images.append(load_image(os.path.join(self.resource_path, r'Idle', f'Idle_{i}.png')))
