a54d5df989de1810d6dd754369398862d0521120
BaerBear <turbogun03@gmail.com>
2025-10-19 01:23:50 +0900

    초기 정의

---
+++ State_Machine.py
@@ -0,0 +1,12 @@
+class StateMachine:
+    def __init__(self, initial_state):
+        pass
+
+    def change_state(self, new_state):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
