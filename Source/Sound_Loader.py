0a5ff4a9db6c1640e50d10806de0ce558e57a4ad
BaerBear <turbogun03@gmail.com>
2025-10-27 00:03:42 +0900

    soundmanager 하나로 통일

--- Source/Sound_Loader.py
+++ Source/Sound_Loader.py
@@ -1,94 +1,6 @@
 import os
 from pico2d import load_wav, load_music
 
-class BGM_Loader:
-    bgms = None
-
-    def load_bgms(cls):
-        if cls.bgms is not None:
-            return cls.bgms
-
-        cls.bgms = {}
-        base_path = os.path.join(os.path.dirname(__file__), r'..\Resources\audio\Bgm')
-
-        cls.bgms['chapter1'] = load_music(os.path.join(base_path, 'Chapter1.wav'))
-        cls.bgms['chapter1_boss'] = load_music(os.path.join(base_path, 'Chapter1_Boss.wav'))
-        cls.bgms['main_title'] = load_music(os.path.join(base_path, 'MainTitle_Hardmode.wav'))
-
-        # 볼륨 조절
-        for bgm in cls.bgms.values():
-            bgm.set_volume(64)
-
-        return cls.bgms
-
-class SKRR_Sound_Loader:
-    sounds = None
-
-    def load_sounds(cls):
-        if cls.sounds is not None:
-            return cls.sounds
-
-        cls.sounds = {}
-        base_path = os.path.join(os.path.dirname(__file__), r'..\Resources\audio\Skul')
-
-        # 플레이어 효과음 로드
-        cls.sounds['jump'] = load_wav(os.path.join(base_path, 'Default_Jump.wav'))
-        cls.sounds['jump_air'] = load_wav(os.path.join(base_path, 'Default_Jump_Air.wav'))
-        cls.sounds['dash'] = load_wav(os.path.join(base_path, 'Default_Dash.wav'))
-        cls.sounds['attack1'] = load_wav(os.path.join(base_path, 'Skul_Atk 1.wav'))
-        cls.sounds['attack2'] = load_wav(os.path.join(base_path, 'Skul_Atk 2.wav'))
-        cls.sounds['jump_attack'] = load_wav(os.path.join(base_path, 'Skul_Jump_Atk.wav'))
-        cls.sounds['dead'] = load_wav(os.path.join(base_path, 'Default_Dead.wav'))
-
-        for sound in cls.sounds.values():
-            sound.set_volume(50)
-
-        return cls.sounds
-
-class Enemy_Sound_Loader:
-    sounds = None
-
-    def load_sounds(cls):
-        if cls.sounds is not None:
-            return cls.sounds
-
-        cls.sounds = {}
-        base_path = os.path.join(os.path.dirname(__file__), r'..\Resources\audio\Enemy')
-
-        for sound in cls.sounds.values():
-            sound.set_volume(50)
-
-        return cls.sounds
-
-class Object_Sound_Loader:
-    sounds = None
-
-    def load_sounds(cls):
-        if cls.sounds is not None:
-            return cls.sounds
-
-        cls.sounds = {}
-        base_path = os.path.join(os.path.dirname(__file__), r'..\Resources\audio\Object')
-
-        for sound in cls.sounds.values():
-            sound.set_volume(50)
-
-        return cls.sounds
-
-class UI_Sound_Loader:
-    sounds = None
-
-    def load_sounds(cls):
-        if cls.sounds is not None:
-            return cls.sounds
-
-        cls.sounds = {}
-        base_path = os.path.join(os.path.dirname(__file__), r'..\Resources\audio\UI')
-
-        for sound in cls.sounds.values():
-            sound.set_volume(50)
-
-        return cls.sounds
 
 class SoundManager:
     _initialized = False
@@ -99,33 +11,92 @@ class SoundManager:
     bgms = None
     current_bgm = None
 
+    @classmethod
     def initialize(cls):
         if cls._initialized:
             return
 
-        cls.player_sounds = SKRR_Sound_Loader.load_sounds()
-        cls.enemy_sounds = Enemy_Sound_Loader.load_sounds()
-        cls.object_sounds = Object_Sound_Loader.load_sounds()
-        cls.ui_sounds = UI_Sound_Loader.load_sounds()
-        cls.bgms = BGM_Loader.load_bgms()
+        audio_path = os.path.join(os.path.dirname(__file__), r'..\Resources\audio')
+        skul_path = os.path.join(audio_path, 'skul')
+
+        # 플레이어 사운드
+        cls.player_sounds = {}
+        cls.player_sounds['Jump'] = load_wav(os.path.join(skul_path, 'Default_Jump.wav'))
+        cls.player_sounds['Jump_air'] = load_wav(os.path.join(skul_path, 'Default_Jump_Air.wav'))
+        cls.player_sounds['Dash'] = load_wav(os.path.join(skul_path, 'Default_Dash.wav'))
+        cls.player_sounds['Attack1'] = load_wav(os.path.join(skul_path, 'Skul_Atk 1.wav'))
+        cls.player_sounds['Attack2'] = load_wav(os.path.join(skul_path, 'Skul_Atk 2.wav'))
+        cls.player_sounds['Jump_attack'] = load_wav(os.path.join(skul_path, 'Skul_Jump_Atk.wav'))
+        cls.player_sounds['Dead'] = load_wav(os.path.join(skul_path, 'Default_Dead.wav'))
+
+        for sound in cls.player_sounds.values():
+            sound.set_volume(64)
+
+        # 적 사운드
+        cls.enemy_sounds = {}
+        cls.enemy_sounds['enemy_dead'] = load_wav(os.path.join(audio_path, 'Enemy_Dead.wav'))
+        cls.enemy_sounds['enemy_big_dead'] = load_wav(os.path.join(audio_path, 'Enemy_Big_Dead.wav'))
+        cls.enemy_sounds['arrow_hit'] = load_wav(os.path.join(audio_path, 'arrow_hit.wav'))
+        cls.enemy_sounds['blast_hit'] = load_wav(os.path.join(audio_path, 'Blast_Hit.wav'))
+
+        for sound in cls.enemy_sounds.values():
+            sound.set_volume(50)
+
+        # 오브젝트 사운드
+        cls.object_sounds = {}
+        cls.object_sounds['gain_gold'] = load_wav(os.path.join(audio_path, 'Object_GainGold.wav'))
+        cls.object_sounds['gain_item'] = load_wav(os.path.join(audio_path, 'Object_GainItem.wav'))
+        cls.object_sounds['open_box'] = load_wav(os.path.join(audio_path, 'Object_OpenBox.wav'))
+
+        for sound in cls.object_sounds.values():
+            sound.set_volume(50)
+
+        # UI 사운드
+        cls.ui_sounds = {}
+        cls.ui_sounds['open'] = load_wav(os.path.join(audio_path, 'UI_Open.wav'))
+        cls.ui_sounds['close'] = load_wav(os.path.join(audio_path, 'UI_Close.wav'))
+        cls.ui_sounds['move'] = load_wav(os.path.join(audio_path, 'UI_Move.wav'))
+        cls.ui_sounds['talk'] = load_wav(os.path.join(audio_path, 'UI_Talk.wav'))
+        cls.ui_sounds['inventory_open'] = load_wav(os.path.join(audio_path, 'UI_Inventory_Open.wav'))
+        cls.ui_sounds['inventory_close'] = load_wav(os.path.join(audio_path, 'UI_Inventory_Close.wav'))
+        cls.ui_sounds['get_ability'] = load_wav(os.path.join(audio_path, 'UI_GetAbility.wav'))
+        cls.ui_sounds['mythology_unlock'] = load_wav(os.path.join(audio_path, 'UI_MythologyUnlock.wav'))
+
+        for sound in cls.ui_sounds.values():
+            sound.set_volume(40)
+
+        # 배경 음악
+        cls.bgms = {}
+        cls.bgms['chapter1'] = load_music(os.path.join(audio_path, 'Chapter1.wav'))
+        cls.bgms['chapter1_boss'] = load_music(os.path.join(audio_path, 'Chapter1_Boss.wav'))
+        cls.bgms['main_title'] = load_music(os.path.join(audio_path, 'MainTitle_Hardmode.wav'))
+
+        for bgm in cls.bgms.values():
+            bgm.set_volume(64)
+
         cls._initialized = True
 
+    @classmethod
     def play_player_sound(cls, sound_name):
         if cls.player_sounds and sound_name in cls.player_sounds:
             cls.player_sounds[sound_name].play()
 
+    @classmethod
     def play_enemy_sound(cls, sound_name):
         if cls.enemy_sounds and sound_name in cls.enemy_sounds:
             cls.enemy_sounds[sound_name].play()
 
+    @classmethod
     def play_object_sound(cls, sound_name):
         if cls.object_sounds and sound_name in cls.object_sounds:
             cls.object_sounds[sound_name].play()
 
+    @classmethod
     def play_ui_sound(cls, sound_name):
         if cls.ui_sounds and sound_name in cls.ui_sounds:
             cls.ui_sounds[sound_name].play()
 
+    @classmethod
     def play_bgm(cls, bgm_name, repeat=True):
         if cls.bgms and bgm_name in cls.bgms:
             if cls.current_bgm:
@@ -137,8 +108,8 @@ class SoundManager:
             else:
                 cls.current_bgm.play()
 
+    @classmethod
     def stop_bgm(cls):
         if cls.current_bgm:
             cls.current_bgm.stop()
             cls.current_bgm = None
-
