0ad9866906c847e950c94b1a813dedd8cdefd0c5
BaerBear <turbogun03@gmail.com>
2025-10-26 20:39:08 +0900

    handle_event 분할

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 from pico2d import *
 from SKRR import *
-
-def handle_events():
-    global running
-    events = get_events()
-    for event in events:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
+from Events import *
 
 def reset_world():
     global world
@@ -30,14 +22,13 @@ def render_world():
     update_canvas()
 
 running = True
-Skrr = None
 
 open_canvas(1440, 900)
 reset_world()
 
 # game loop
 while running:
-    handle_events()
+    handle_events(running, Skrr)
     update_world()
     render_world()
     delay(0.01)
