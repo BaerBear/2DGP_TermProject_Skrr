3486ce33626f961b4171a50dcfa480ab9cb76c43
BaerBear <turbogun03@gmail.com>
2025-10-26 22:52:57 +0900

    main내 world에 테스트용 몬스터 추가

--- Source/main.py
+++ Source/main.py
@@ -1,15 +1,28 @@
 from pico2d import *
 from SKRR import *
+from Enemy import Knight_Sword, Knight_Bow, Knight_Tackle
 import Events
 
 def reset_world():
-    global world
+    global world, enemies
     world = []
 
     global Skrr
     Skrr = SKRR()
-
     world.append(Skrr)
+
+    # 테스트용 몬스터 생성
+    sword_knight = Knight_Sword(900, get_canvas_height() // 2)
+    sword_knight.target = Skrr
+    world.append(sword_knight)
+
+    bow_knight = Knight_Bow(1100, get_canvas_height() // 2)
+    bow_knight.target = Skrr
+    world.append(bow_knight)
+
+    tackle_knight = Knight_Tackle(700, get_canvas_height() // 2)
+    tackle_knight.target = Skrr
+    world.append(tackle_knight)
 
 def update_world():
     for obj in world:
