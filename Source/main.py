49aa460251c3cb4e40b68b775249590fe3bab371
BaerBear <turbogun03@gmail.com>
2025-10-31 18:08:30 +0900

    main에 game_world 적용

--- Source/main.py
+++ Source/main.py
@@ -3,38 +3,37 @@ from SKRR import *
 from Enemy import Knight_Sword, Knight_Bow, Knight_Tackle
 from Sound_Loader import SoundManager
 import Events
+import game_world
 
 def reset_world():
     SoundManager.initialize()
     SoundManager.play_bgm('chapter1', repeat=True)
-    global world, enemies
-    world = []
+
     global Skrr
     Skrr = SKRR()
 
-    # 테스트용 몬스터 생성
+    # Layer 2: 플레이어
+    game_world.add_object(Skrr, 2)
+
+    # Layer 1: 적(Enemy)
     sword_knight = Knight_Sword(900, get_canvas_height() // 2)
     sword_knight.target = Skrr
-    world.append(sword_knight)
+    game_world.add_object(sword_knight, 1)
 
     bow_knight = Knight_Bow(1100, get_canvas_height() // 2)
     bow_knight.target = Skrr
-    world.append(bow_knight)
+    game_world.add_object(bow_knight, 1)
 
     tackle_knight = Knight_Tackle(700, get_canvas_height() // 2)
     tackle_knight.target = Skrr
-    world.append(tackle_knight)
-
-    world.append(Skrr)
+    game_world.add_object(tackle_knight, 1)
 
 def update_world():
-    for obj in world:
-        obj.update()
+    game_world.update()
 
 def render_world():
     clear_canvas()
-    for obj in world:
-        obj.draw()
+    game_world.render()
     update_canvas()
 
 running = True
