6ec49b78ece5e83c8c910e11fc4e95987c9bffb5
BaerBear <turbogun03@gmail.com>
2025-11-06 20:25:39 +0900

    델타타임 추가

--- Source/game_framework.py
+++ Source/game_framework.py
@@ -1,3 +1,6 @@
+import time
+
+frame_time = 0.0
 running = None
 stack = None
 
@@ -40,18 +43,25 @@ def quit():
 
 
 def run(start_mode):
-    global running, stack
+    global running, stack, frame_time
     running = True
     stack = [start_mode]
     start_mode.init()
+
+    frame_time = 0.0
+    current_time = time.time()
 
     while running:
         stack[-1].handle_events()
         stack[-1].update()
         stack[-1].draw()
 
+        frame_time = time.time() - current_time
+        frame_rate = 1.0 / frame_time if frame_time > 0 else 0
+        current_time += frame_time
+        # print(f'Frame Time: {frame_time:.4f}, Frame Rate: {frame_rate:.2f}')
+
     # repeatedly delete the top of the stack
     while (len(stack) > 0):
         stack[-1].finish()
         stack.pop()
-
