d1433ae502e040bc033314440d4c62ee450e9de7
BaerBear <turbogun03@gmail.com>
2025-12-10 20:58:33 +0900

    경로 변경

--- Source/main.py
+++
@@ -1,9 +0,0 @@
-from pico2d import *
-from Sound_Loader import SoundManager
-import logo_mode as start_mode
-import game_framework
-
-open_canvas(game_framework.width, game_framework.height)
-SoundManager.initialize()
-game_framework.run(start_mode)
-close_canvas()
